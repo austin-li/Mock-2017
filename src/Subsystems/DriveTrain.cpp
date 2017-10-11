df9580da20c3bfe16e312e5cefd3ced1ce65d15e
LAPTOP-A4KE1I09\Austin <austinmingli@gmail.com>
2017-10-10 19:39:56 -0700

    DriveTrain subsystem with motor, ultrasonic sensor, and getUltra method.

---
+++ src/Subsystems/DriveTrain.cpp
@@ -0,0 +1,18 @@
+#include "DriveTrain.h"
+#include "../RobotMap.h"
+
+DriveTrain::DriveTrain() : Subsystem("DriveTrain"), driveMotor(new Jaguar(MOTORPORT)), ultrasonicSensor(new Ultrasonic(ULTRASONIC_TRIGGER, ULTRASONIC_ECHO)) {
+	ultrasonicSensor->SetAutomaticMode(true);
+}
+
+void DriveTrain::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+
+double DriveTrain::getUltra() {
+	return ultrasonicSensor->GetRangeInches();
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
