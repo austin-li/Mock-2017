1bf4974fc03bd3ef7321ff40278b1cc7e4f86778
LAPTOP-A4KE1I09\Austin <austinmingli@gmail.com>
2017-11-10 21:18:28 -0800

    OI with 2 joysticks

--- src/OI.cpp
+++ src/OI.cpp
@@ -2,6 +2,14 @@
 
 #include <WPILib.h>
 
-OI::OI() {
+OI::OI() :leftStick(new Joystick(0)), rightStick(new Joystick(1)){
 	// Process operator interface input here.
+}
+
+Joystick* OI::getLeft() {
+	return leftStick;
+}
+
+Joystick* OI::getRight() {
+	return rightStick;
 }
